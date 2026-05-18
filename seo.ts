4a771b5c76ffc0b8015c911c50944499b32696a1
Justin Ray <145301348+Loserdub@users.noreply.github.com>
2026-05-18 14:47:50 -0400

    html convert
    remove react

--- seo.ts
+++
@@ -1,6 +0,0 @@
-const BASE = 'https://jray.me';
-
-export const buildPageUrl = (pathname: string): string => {
-  const clean = pathname === '/' ? '' : pathname.replace(/\/$/, '');
-  return `${BASE}${clean}`;
-};
